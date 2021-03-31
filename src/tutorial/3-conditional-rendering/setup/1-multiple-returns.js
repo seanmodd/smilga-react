fb1fa567e2641eef95f28dd91a00a417dbba6764
Sean <sean@vestaridefair.com>
2021-03-31 10:45:26 -0700

    ok

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { Heading } from '@chakra-ui/layout';
 import React, { useState, useEffect } from 'react';
 
@@ -7,13 +8,35 @@ const MultipleReturns = () => {
   const [isError, setIsError] = useState(false);
   const [user, setUser] = useState('default user');
 
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((user) => {
+        const { login } = user;
+        setUser(login);
+        setIsLoading(false);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   if (isLoading) {
-    return <Heading>Loading...</Heading>;
+    return (
+      <>
+        <Heading>Loading...</Heading>
+      </>
+    );
+  }
+  if (isError) {
+    return (
+      <>
+        <Heading>Error...</Heading>
+      </>
+    );
   }
   return (
     <>
       <Heading color="pink.300" my="35px" fontSize="xl">
-        Multiple Returns Starter
+        {user}
       </Heading>
     </>
   );
